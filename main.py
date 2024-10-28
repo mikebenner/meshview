e88fe9b66c2f67f5de8a7cb7e66a7a2fd08f8e67
Mike Benner <36419818+mikebenner@users.noreply.github.com>
2024-10-27 22:07:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ from meshview import store
 from meshview import web
 from meshview import http
 
-
 async def load_database_from_mqtt(mqtt_server, topic):
     async for topic, env in mqtt_reader.get_topic_envelopes(mqtt_server, topic):
         await store.process_envelope(topic, env)
 
-
 async def main(args):
     database.init_database(args.database)
-
     await database.create_tables()
     async with asyncio.TaskGroup() as tg:
         tg.create_task(load_database_from_mqtt(args.mqtt_server, args.topic))
@@ -23,19 +20,22 @@ async def main(args):
         if args.acme_challenge:
             tg.create_task(http.run_server(args.bind, args.acme_challenge))
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('meshview')
-    parser.add_argument('--bind', nargs='*', default=['::1'])
+    parser.add_argument('--bind', nargs='*', default=['::1'])  # Listen on all IPs
     parser.add_argument('--acme-challenge')
-    parser.add_argument('--port', default=8000, type=int)
+    parser.add_argument('--port', default=8000, type=int)  # Ensure default is 8000
     parser.add_argument('--tls-cert')
-
     parser.add_argument('--mqtt-server', default='mqtt.mbenner.com')
     parser.add_argument('--topic', nargs='*', default=['msh/US/#'])
-
     parser.add_argument('--database', default='sqlite+aiosqlite:///packets.db')
 
     args = parser.parse_args()
 
-    asyncio.run(main(args))
+    # Explicitly create and manage the event loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main(args))
+    finally:
+        loop.close()
